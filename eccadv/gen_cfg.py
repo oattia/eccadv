97add522070c34af83ec9340749f68c66216fdf1
oy.attia@gmail.com <oy.attia@gmail.com>
2019-04-15 18:46:50 -0400

    adding big experiment for rs

--- eccadv/gen_cfg.py
+++ eccadv/gen_cfg.py
@@ -16,31 +16,34 @@ from collections import OrderedDict
 
 def main():
     models = ["ch_cnn_ce",
-              # "ch_cnn_mae",
+              "ch_cnn_mae",
               "ch_cnn_be"]
+
     attackers = [
                 # "dummy",
                  # "fgsm_ch_0.05", "fgsm_ch_0.1", "fgsm_ch_0.2",
                  "fgsm_ch_0.3"]
+
     pro_methods = OrderedDict([
-        ("oh", [
-                # ("dummy", 1),
-                ("rep_11_block", 11),
-                # ("rep_11_bit", 11),
-                # ("rep_5_block", 5), ("rep_5_bit", 5)
-                ]),
-        ("weight_3_6", [("rep_11_block", 33),
-                        # ("rep_11_bit", 33),
-                        # ("rep_5_block", 15), ("rep_5_bit", 15)
-                        ]),
-        # ("rand_5", [("hd", 16)]),
-        ("rand_10", [("hd", 512)]),
-        # ("dummy_rs_3_12", [("dummy", 12)]),
-        ("dummy_rs_4_16", [("dummy", 16)]),
-        # ("dummy_rs_4_6", [("dummy", 6)])
+        # ("oh", [
+        #         # ("dummy", 1),
+        #         ("rep_11_block", 11),
+        #         # ("rep_11_bit", 11),
+        #         # ("rep_5_block", 5), ("rep_5_bit", 5)
+        #         ]),
+        # ("weight_3_6", [("rep_11_block", 33),
+        #                 # ("rep_11_bit", 33),
+        #                 # ("rep_5_block", 15), ("rep_5_bit", 15)
+        #                 ]),
+        # # ("rand_5", [("hd", 16)]),
+        # ("rand_10", [("hd", 512)]),
+        # # ("dummy_rs_3_12", [("dummy", 12)]),
+        # ("dummy_rs_4_16", [("dummy", 16)]),
+        # # ("dummy_rs_4_6", [("dummy", 6)])
+        ("dummy_rs_128_512", [("dummy", 512)])
     ])
 
-    exp = 1
+    exp = 800
     for model in models:
         for attacker in attackers:
             for pro_method in pro_methods.items():
